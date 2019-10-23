ef2151b7b0d23f8090989050a00f142aaaadcc25
Min2208 <vuong.bkacad@gmail.com>
2019-10-23 22:33:24 +0700

    thuc hanh typescript

---
+++ demo.js
@@ -0,0 +1,11 @@
+function main() {
+    var x = 5;
+    console.log(x);
+    var y = 10;
+    console.log(x);
+    var a = 55;
+    console.log(a);
+    var b = 100;
+    console.log(b);
+}
+main();
---
+++ demo.ts
@@ -0,0 +1,13 @@
+
+function main() {
+    var x = 5;
+    console.log(x);
+    var y = 10;
+    console.log(x);
+
+    let a = 55;
+    console.log(y);
+    let b = 100;
+    console.log(y);
+}
+main()
